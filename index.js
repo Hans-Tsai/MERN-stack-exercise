6a8f7719ec576116c13299bc16eddfdbe4617314
Hans <lgs840522@gmail.com>
2021-02-12 23:17:19 +0800

    feat(database): finish database MongoDB connection settings

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // connect to the database
-mongoose.connect(process.env.DB, { useNewUrlParser: true })
+mongoose.connect(process.env.DB, { useNewUrlParser: true , useUnifiedTopology: true})
   .then(() => console.log(`Database connected successfully`))
   .catch(err => console.log(err));
 
